480e0b88b82d787d600ad7643f262cf2d4e89667
Michael Maximenko <Mishamax2007@gmail.com>
2022-08-21 06:27:36 +0300

    dd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Container } from "react-bootstrap"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
@@ -6,9 +7,13 @@ function App() {
   return (
     <div className='App'>
       <Header />
+
       <main>
-        <h1>Welcome to MyShop</h1>
+        <Container>
+          <h1>Welcome to MyShop</h1>
+        </Container>
       </main>
+
       <Footer />
     </div>
   )
