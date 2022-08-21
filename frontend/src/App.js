99e7950c0f4ec7ad0816d7f3f6f1958e83e0f8bb
Michael Maximenko <Mishamax2007@gmail.com>
2022-08-21 06:12:49 +0300

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <h1>Welcome to MyShop</h1>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
