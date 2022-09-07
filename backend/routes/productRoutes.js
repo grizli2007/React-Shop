3471111920221a4d7b902d291de14ba9fdcda1cf
Michael Maximenko <Mishamax2007@gmail.com>
2022-09-07 14:25:32 +0300

    .

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,8 +3,6 @@ import asyncHandler from "express-async-handler"
 const router = express.Router()
 import Product from "../models/productModel.js"
 
-
-
 router.get(
   "/",
   asyncHandler(async (req, res) => {
@@ -13,15 +11,18 @@ router.get(
   })
 )
 
-router.get('/:id',asyncHandler(async(req,res)=>{
-  const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-  if(product){
-    res.json(product)
-  }else{
-    res.status(404)
-    throw new Error('Product not found')
-  }
-}))
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error("Product not found")
+    }
+  })
+)
 
 export default router
