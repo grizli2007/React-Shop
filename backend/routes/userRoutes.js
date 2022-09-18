eb49de18ca5d513f131c78f39412e46d502c0f88
Michael Maximenko <Mishamax2007@gmail.com>
2022-09-18 05:55:18 +0300

    4

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,8 @@
 import express from "express"
 const router = express.Router()
-import {
-  authUser
-} from "../controllers/userController.js"
+import { authUser, getUserProfile } from "../controllers/userController.js"
+import { authorizer } from "../middleware/authMiddleware.js"
 
-router.post('/login',authUser)
-
+router.post("/login", authUser)
+router.route("/profile").get(authorizer, getUserProfile)
 export default router
