82cf8ba9367e734f85e050dad97bc5c77f60ffed
Michael Maximenko <Mishamax2007@gmail.com>
2022-10-02 06:37:42 +0300

    ,

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from "express"
 const router = express.Router()
-import { addOrderItems, getOrderById } from "../controllers/orderController.js"
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js"
 import { authorizer } from "../middleware/authMiddleware.js"
 
 router.route("/").post(authorizer, addOrderItems)
 router.route("/:id").get(authorizer, getOrderById)
+router.route("/:id/pay").put(authorizer, updateOrderToPaid)
 
 export default router
--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -108,44 +108,46 @@ const PlaceOrderScreen = ({ history }) => {
 
         <Col md={4}>
           <Card>
-            <ListGroup.Item>
-              <h2>Order Summary</h2>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Items</Col>
-                <Col>${cart.itemsPrice}</Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Shipping</Col>
-                <Col>${cart.shippingPrice}</Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Tax</Col>
-                <Col>${cart.taxPrice}</Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Total</Col>
-                <Col>${cart.totalPrice}</Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              {error && <Message variant='danger'>{error}</Message>}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Button
-                type='button'
-                className='btn-block'
-                disabled={cart.cartItems === 0}
-                onClick={placeOrderHandler}
-              ></Button>
-            </ListGroup.Item>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {error && <Message variant='danger'>{error}</Message>}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  type='button'
+                  className='btn-block'
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                ></Button>
+              </ListGroup.Item>
+            </ListGroup>
           </Card>
         </Col>
       </Row>
